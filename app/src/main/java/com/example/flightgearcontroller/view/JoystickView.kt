ee724818a2b532691777f5b485024f6351a28bec
Itay <iyacov@gmail.com>
2021-06-24 21:15:09 +0300

    working app - no network 2

--- app/src/main/java/com/example/flightgearcontroller/view/JoystickView.kt
+++ app/src/main/java/com/example/flightgearcontroller/view/JoystickView.kt
@@ -1,4 +1,230 @@
 package com.example.flightgearcontroller.view
 
-class JoystickView {
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import com.example.flightgearcontroller.R
+import kotlin.math.*
+
+
+class JoystickView : View {
+    val colorArray = intArrayOf(Color.RED, Color.GREEN, Color.BLUE)
+    val positionArray = floatArrayOf(0F, 0.5F, 0.6F)
+    val startColor = Color.RED
+    val endColor = Color.BLUE
+    private var mContext: Context? = context
+    private var mLayout: ViewGroup? = null
+    private var params: ViewGroup.LayoutParams? = null
+    private var draw: DrawCanvas? = null
+    private var stick: Bitmap? = null
+    private var mEnabled: Boolean = false
+
+    private var touchState = false
+
+    private var STICK_ALPHA = 200
+    private var LAYOUT_ALPHA = 200
+    private var OFFSET = 0
+
+    private var stickWidth = 0
+    private var stickHeight = 0
+    private var layoutWidth = 0
+    private var layoutHeight = 0
+    private var positionX = 0
+    private var positionY = 0
+    private var maxX: Float = 0f
+    private var maxY: Float = 0f
+    private var minDistance: Float = 0f
+    private var maxDistance: Float = 0f
+    private var distance = 0f
+    private var angle = 0f
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    fun createJoystick(layout: ViewGroup?) {
+        layout?.let {
+            mLayout = layout
+            stick = BitmapFactory.decodeResource(mContext!!.resources, R.drawable.joystick_in)
+            stick = Bitmap.createScaledBitmap(stick!!, stickWidth, stickHeight, false)
+            draw = DrawCanvas(mContext)
+            params = mLayout!!.layoutParams
+            params!!.width = layoutWidth
+            params!!.height = layoutHeight
+
+            maxDistance = sqrt(maxX.toDouble().pow(2.0) + maxY.toDouble().pow(2.0)).toFloat()
+
+            layout.setOnTouchListener { v: View, e: MotionEvent ->
+                drawStick(e)
+                if (e.action == MotionEvent.ACTION_UP) {
+                    v.performClick()
+                }
+                true
+            }
+//        }
+            mEnabled = true;
+        }
+    }
+
+    fun getOffset(): Int {
+        return OFFSET
+    }
+
+    fun setOffset(offset: Int) {
+        OFFSET = offset
+    }
+
+    fun getJoystickX(): Int {
+        if (distance > this.minDistance && touchState) {
+            return positionX * (1)
+        } else {
+            return 0
+        }
+    }
+
+    fun getJoystickY(): Int {
+        if (distance > this.minDistance && touchState) {
+            return positionY * (-1)
+        } else {
+            return 0
+        }
+    }
+
+    fun setMinimumDistance(minDistance: Float) {
+        this.minDistance = minDistance
+    }
+
+    fun getDistance(): Float {
+        if (distance > this.minDistance && touchState) {
+            return distance
+        } else {
+            return 0F
+        }
+    }
+
+    fun getAngle(): Float {
+        if (distance > this.minDistance && touchState) {
+            return angle
+        } else {
+            return 0F
+        }
+    }
+
+    fun setStickSize(width: Int, height: Int) {
+        stickWidth = width
+        stickHeight = height
+    }
+
+    fun setLayoutSize(width: Int, height: Int) {
+        layoutWidth = width
+        layoutHeight = height
+        maxX = (width / 2 - OFFSET).toFloat()
+        maxY = (height / 2 - OFFSET).toFloat()
+    }
+
+    fun getLayoutWidth(): Int {
+        return params!!.width
+    }
+
+    fun getLayoutHeight(): Int {
+        return params!!.height
+    }
+
+    fun getPosition(): IntArray? {
+        if (distance > this.minDistance && touchState) {
+            return intArrayOf(positionX, positionY)
+        } else {
+            return intArrayOf(0, 0)
+        }
+    }
+
+    private fun drawStick(event: MotionEvent) {
+        positionX = (event.x - params!!.width / 2).toInt()
+        positionY = (event.y - params!!.height / 2).toInt()
+        distance = sqrt(positionX.toDouble().pow(2.0) + positionY.toDouble().pow(2.0)).toFloat()
+        distance = min(distance, maxDistance)
+        angle = calculateAngle(positionX.toFloat(), (positionY * (-1)).toFloat()).toFloat()
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                if (distance <= maxX) {
+                    draw!!.position(event.x.toDouble(), event.y.toDouble())
+                    draw()
+                    touchState = true
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (touchState) {
+                    var x = 0f
+                    var y = 0f
+                    if (distance <= maxX) {
+                        x = event.x
+                        y = event.y
+                    } else if (distance > maxX) {
+                        val posAngle = calculateAngle(positionX.toFloat(), positionY.toFloat())
+                        x = (cos(Math.toRadians(posAngle)) * maxX).toFloat()
+                        y = (sin(Math.toRadians(posAngle)) * maxY).toFloat()
+                        x += (params!!.width / 2).toFloat()
+                        y += (params!!.height / 2).toFloat()
+                    }
+                    draw!!.position(x.toDouble(), y.toDouble())
+                    draw()
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                mLayout!!.removeView(draw)
+                touchState = false
+            }
+        }
+    }
+
+    private fun calculateAngle(x: Float, y: Float): Double {
+        if (x >= 0 && y >= 0) {
+            return Math.toDegrees(atan(y / x.toDouble()))
+        } else if (x < 0 && y >= 0) {
+            return Math.toDegrees(atan(y / x.toDouble())) + 180
+        } else if (x < 0 && y < 0) {
+            return Math.toDegrees(atan(y / x.toDouble())) + 180
+        } else if (x >= 0 && y < 0) {
+            return Math.toDegrees(atan(y / x.toDouble())) + 360
+        }
+        return 0.0
+    }
+
+    private fun draw() {
+        try {
+            mLayout!!.removeView(draw)
+        } catch (e: Exception) {
+        }
+        mLayout!!.addView(draw)
+    }
+
+    inner class DrawCanvas constructor(mContext: Context?) :
+        View(mContext) {
+        var x: Double = 0.0
+        var y: Double = 0.0
+        private var paint: Paint = Paint()
+        private var paint2: Paint = Paint()
+
+        public override fun onDraw(canvas: Canvas) {
+            canvas.drawBitmap(stick!!, x.toFloat(), y.toFloat(), paint)
+
+            paint2.color = Color.BLACK
+            paint2.textSize = 40f
+            canvas.drawText("DIS=%f".format(distance), x.toFloat(), y.toFloat(), paint2)
+            canvas.drawText("ANG=%f".format(angle), x.toFloat(), y.toFloat() - 30, paint2)
+        }
+
+        fun position(pos_x: Double, pos_y: Double) {
+            x = pos_x - stickWidth / 2
+            y = pos_y - stickHeight / 2
+        }
+    }
 }
--- app/src/main/java/com/example/flightgearcontroller/view/MainActivity.kt
+++ app/src/main/java/com/example/flightgearcontroller/view/MainActivity.kt
@@ -1,11 +1,71 @@
-package com.example.flightgearcontroller
+package com.example.flightgearcontroller.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.flightgearcontroller.R
+
 
 class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+
+    var relativeLayoutJoystick: RelativeLayout? = null
+    var inner: ImageView? = null
+    var outer: ImageView? = null
+
+    var joystick: JoystickView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//
+//        relativeLayoutJoystick = findViewById<View>(R.id.relativeLayoutJoystick) as RelativeLayout
+////
+////         joystick = Joystick(applicationContext, relativeLayoutJoystick!!, R.drawable.joystick_in)
+//        joystick = JoystickView(applicationContext)
+//        joystick!!.setOffset(80)
+//        joystick!!.setStickSize(150, 150)
+////        joystick!!.setLayoutSize(850, 850)
+//        joystick!!.setLayoutSize(250, 250)
+//        joystick!!.setMinimumDistance(20f)
+//        joystick!!.createJoystick(relativeLayoutJoystick!!)
+
+//         relativeLayoutJoystick!!.setOnTouchListener { arg0, arg1 -> joystick!!.drawStick(arg1)
+//        //     if (arg1.action == MotionEvent.ACTION_DOWN || arg1.action == MotionEvent.ACTION_MOVE) {
+//        //         val x = "X : " + joystick!!.getX().toString()
+//        //         val y = "Y : " + joystick!!.getY().toString()
+//        //         val angle = "Angle : " + joystick!!.getAngle().toString()
+//        //         val distance = "Distance : " + joystick!!.getDistance().toString()
+//        //     } else if (arg1.action == MotionEvent.ACTION_UP) {
+//
+//             }
+//             true;
+//         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        relativeLayoutJoystick = findViewById<View>(R.id.relativeLayoutJoystick) as RelativeLayout
+
+        relativeLayoutJoystick?.post {
+            val layoutWidth = relativeLayoutJoystick!!.width
+            val layoutHeight = relativeLayoutJoystick!!.height
+            val squaredSize = Math.min(layoutWidth, layoutHeight).toInt()
+            val squaredSmallSize = (0.25f * squaredSize).toInt()
+            val offset = (squaredSmallSize / 2f).toInt()
+            joystick = JoystickView(applicationContext)
+            joystick!!.setOffset(offset)
+            joystick!!.setStickSize(squaredSmallSize, squaredSmallSize)
+            joystick!!.setLayoutSize(squaredSize, squaredSize)
+            joystick!!.setMinimumDistance(20f)
+            joystick!!.createJoystick(relativeLayoutJoystick)
+        }
+    }
+
 }
